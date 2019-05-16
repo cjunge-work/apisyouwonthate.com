ab09e9493a8cecd8739011f0dba4b7528f3e3e42
Mike Bifulco <mbifulco@live.com>
2019-05-16 08:56:32 -0400

    fix image size

--- src/components/AuthorSummary/AuthorSummary.js
+++ src/components/AuthorSummary/AuthorSummary.js
@@ -9,6 +9,7 @@ import { GitHubIcon, InstagramIcon, TwitterIcon } from '../icons';
 import { Image } from '..';
 
 import classes from './AuthorSummary.module.css';
+import { Button } from '../Button';
 
 const AuthorSummary = ({ author }) => {
   console.log('with author', author);
@@ -24,54 +25,55 @@ const AuthorSummary = ({ author }) => {
   return (
     <div className={classes.container}>
       <Row noGutters>
-        <Col xs={2} sm={1}>
-          {photo && (
-            <div className={classes.headshotContainer}>
-              <Image src={photo} />
+        <Col xs={12}>
+          <header className={classes.header}>
+            {photo && (
+              <div className={classes.headshotContainer}>
+                <Image src={photo} />
+              </div>
+            )}
+            <div>
+              <Link to={`/authors/${slugify(name)}`}>
+                <h2>{name}</h2>
+              </Link>
+              <ul className={classes.socialLinks}>
+                <li>
+                  {github && (
+                    <OutboundLink
+                      href={`https://github.com/${github}`}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <GitHubIcon /> @{github}
+                    </OutboundLink>
+                  )}
+                </li>
+                <li>
+                  {twitter && (
+                    <OutboundLink
+                      href={`https://twitter.com/${twitter}`}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <TwitterIcon /> @{twitter}
+                    </OutboundLink>
+                  )}
+                </li>
+                <li>
+                  {instagram && (
+                    <OutboundLink
+                      href={`https://instagram.com/${instagram}`}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <InstagramIcon /> @{instagram}
+                    </OutboundLink>
+                  )}
+                </li>
+              </ul>
             </div>
-          )}
-        </Col>
-        <Col xs={10} sm={11}>
-          <div>
-            <Link to={`/authors/${slugify(name)}`}>
-              <h2>{name}</h2>
-            </Link>
-            <ul className={classes.socialLinks}>
-              <li>
-                {github && (
-                  <OutboundLink
-                    href={`https://github.com/${github}`}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <GitHubIcon /> @{github}
-                  </OutboundLink>
-                )}
-              </li>
-              <li>
-                {twitter && (
-                  <OutboundLink
-                    href={`https://twitter.com/${twitter}`}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <TwitterIcon /> @{twitter}
-                  </OutboundLink>
-                )}
-              </li>
-              <li>
-                {instagram && (
-                  <OutboundLink
-                    href={`https://instagram.com/${instagram}`}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <InstagramIcon /> @{instagram}
-                  </OutboundLink>
-                )}
-              </li>
-            </ul>
-          </div>
+            <Button>Consulting</Button>
+          </header>
         </Col>
       </Row>
       <Row>
